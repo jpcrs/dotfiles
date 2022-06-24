726d5e920a8254e6eb3ea54e8b6a2978c7151cc6
jpcrs <joaop.carrasco@gmail.com>
2022-06-24 17:45:39 +0100

    :nail_care:

--- .config/nvim/lua/user/lsp/settings/omnisharp.lua
+++
@@ -1,8 +0,0 @@
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/home/jpcrs/bin/omnisharp"
-
-return {
-	settings = {
-        cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
-	},
-}
